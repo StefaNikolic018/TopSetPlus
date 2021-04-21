aaaa4c157d7ca8a79358301df19fc97a4cb16161
Stefan Nikolic <stefanikolic018@gmail.com>
2021-04-21 13:27:15 +0200

    gallery added

--- js/app.js
+++ js/app.js
@@ -11,3 +11,14 @@ window.onscroll = function () {
     myNav.classList.remove("scroll");
   }
 };
+
+// JavaScript Document
+$(document).ready(function () {
+  $("#autoWidth").lightSlider({
+    autoWidth: true,
+    loop: true,
+    onSliderLoad: function () {
+      $("#autoWidth").removeClass("cS-hidden");
+    },
+  });
+});
